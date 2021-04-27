3a12fd1692555826dca0ee9eed56aca815850455
pavitradalawai <83263125+pavitradalawai@users.noreply.github.com>
2021-04-27 09:44:54 +0530

    Add files via upload

---
+++ Primitive1.java
@@ -0,0 +1,29 @@
+public class Primitive1{
+public static void main(String[]args){
+byte myNum=120;
+System.out.println(myNum);
+Short number=6000;
+System.out.println(number);
+
+int number2=200000;
+System.out.println(number2);
+
+long number3=300000000L;
+System.out.println(number3);
+
+float number4=1.82f;
+System.out.println(number4);
+
+double number5=18.88d;
+System.out.println(number5);
+
+char myName='M';
+System.out.println(myName);
+
+
+boolean study=true;
+boolean nostudy=false;
+System.out.println(study);
+System.out.println(nostudy);
+}
+}
